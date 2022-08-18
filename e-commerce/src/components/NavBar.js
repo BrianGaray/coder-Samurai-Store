d552fb5ec508be03c2c3b96f795c22e56a1a7d2f
BrianGaray <brianchess18@gmail.com>
2022-08-17 21:52:16 -0300

    segunda entrega (NavBar)

---
+++ e-commerce/src/components/NavBar.js
@@ -0,0 +1,27 @@
+function Nav() {
+  return (
+    <div class="container">
+      <div class="navbar">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
